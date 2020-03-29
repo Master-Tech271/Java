95d45630c3dbe1373e5c5c65d4eeaea182b792b1
Master-Tech271 <umar17605@gmail.com>
2020-03-29 19:04:54 +0530

    start

---
+++ ArrayList1.java
@@ -0,0 +1,18 @@
+//package com.collectionframework;
+//import java.util.ArrayList;
+//import java.util.List;
+//public class ArrayList1 {
+//	public void arrayList() {
+//		ArrayList fruits = new ArrayList<>();
+//		fruits.add("Apple");
+//		fruits.add(123);
+//		System.out.println(fruits);
+//	}
+//	public void arrayListMethods() {
+////		List<String> Vegetables = new ArrayList<>();
+////		Vegetables.add("carrot");
+////		Vegetables.add("reddish");
+////		Vegetables.clear();
+//		
+//	}
+//}
